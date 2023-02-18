f868b5af4ffb6d8e96250198da4e349d63dddbb4
Gauravcodeit <adhikarigaurav82@gmail.com>
2023-02-18 21:44:03 +0530

    Added a audio file

--- src/Components/SongCard.js
+++ src/Components/SongCard.js
@@ -1,7 +1,6 @@
 import React from 'react'
-
+import firstAudio from '../Asset/first-audio.mp3';
 export default function songCard(props) {
-
 const {count,setcount}=props;
 const increment = () => {
   setcount(count + 1)
@@ -11,7 +10,13 @@ const increment = () => {
     <div  className='Card-Title'>
      CARD OF MAGIC
     </div>
-
+    <audio
+        controls
+        src={firstAudio}>
+            <a href={firstAudio}>
+                Download audio
+            </a>
+    </audio>
     <div>Song Name: {props.songName}</div>
     <div>Singer Name: {props.singerName}</div>
     <div>Views:{count}</div>
